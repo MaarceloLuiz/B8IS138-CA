1accc0633bb3bc8d1ff7b123b87cc4b973671329
MaarceloLuiz <marceloluizgpereira@gmail.com>
2025-10-28 12:53:08 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'B8IS138-CA',
+  webDir: 'www'
+};
+
+export default config;
