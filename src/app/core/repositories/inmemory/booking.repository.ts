08dbb98563d6bbbdb37c8ee4519b33bda06dbfe6
MaarceloLuiz <marceloluizgpereira@gmail.com>
2025-10-30 10:25:10 +0000

    feat: create data models, folder strucutre and mock data

---
+++ src/app/core/repositories/inmemory/booking.repository.ts
@@ -0,0 +1,5 @@
+/**
+ * In-memory implementation of BookingRepository
+ * Stores bookings in memory for CA One
+ * Will be replaced with Firestore DB implementation in CA Two
+*/
---
+++ src/app/core/repositories/inmemory/inmemory-property.repository.ts
@@ -0,0 +1,5 @@
+/**
+ * In-memory implementation of PropertyRepository
+ * Uses mock data from JSON file for CA One
+ * Will be replaced with Firestore DB implementation in CA Two
+*/
