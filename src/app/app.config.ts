6016fad8f05f89adcfc44bd75ec9bbb03fb434c5
MaarceloLuiz <marceloluizgpereira@gmail.com>
2025-11-17 17:38:43 +0000

    chore: update app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,17 +6,11 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 
-/**
- * app config
- * uses env vars for Firebase config
- * loads env vars from ../environments/environment.ts
-*/
-
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(routes),
-        provideIonicAngular(),
-        provideFirebaseApp(() => initializeApp(environment.firebase)),
-        provideAuth(() => getAuth()),
-    ]
+  providers: [
+    provideRouter(routes),
+    provideIonicAngular(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+  ]
 };
